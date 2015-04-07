0e8a36fae0bb1c257e0d5038dd688af7248e0029
alnutile <alfrednutile@gmail.com>
2015-04-06 22:06:53 -0400

    How to fix merge conflict

--- setup/set_env.php
+++ setup/set_env.php
@@ -6,18 +6,12 @@ use Symfony\Component\Filesystem\Filesystem;
 
 try
 {
-<<<<<<< HEAD
     $filesystem = new Filesystem();
     if($filesystem->exists('.env'))
     {
         echo sprintf("Env file exists already %s", __DIR__ . '/../.env');
     } else {
         echo sprintf("Copied .env file too root");
-=======
-    if(!file_exists(__DIR__.'/../.env'))
-    {
->>>>>>> 1505bfb119affd1e7d4769d3bac2885d047d54d5
-        exec("cp env.example .env");
     }
 }
 catch(\Exception $e)
